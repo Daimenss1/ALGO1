65c4ee7c3c05f20932510f3abe39f36205f94b6e
dgobozov <saniamit@mail.ru>
2022-05-18 15:39:39 +0300

    Algoritmy

--- src/test/IntListenImpl.java
+++ src/test/IntListenImpl.java
@@ -1,4 +1,98 @@
 package test;
 
-public class IntListenImpl {
-}
+public class IntListenImpl implements Listen {
+
+    private Integer[] storage;
+    private static final int DEFAULT_CAPACITY = 100;
+    private int size;
+
+    IntListenImpl(){
+        this.storage = new Integer[DEFAULT_CAPACITY];
+    }
+
+
+
+    @Override
+    public String add (Integer item){
+        checkIfNull(item);
+        if (size == storage.length){
+            storage = growStorage();
+        }
+        return null;
+    }
+
+    private Integer[] growStorage() {
+    }
+
+    private void checkIfNull(Integer item) {
+        if (item == null) {
+            throw new NextNullException("вводимые параметры NULL");
+        }
+
+
+
+        @Override
+        public String add ( int index, Integer item){
+            return null;
+        }
+
+        @Override
+        public String set ( int index, Integer item){
+            return null;
+        }
+
+        @Override
+        public String remove (Integer item){
+            return null;
+        }
+
+        @Override
+        public String remove ( int index){
+            return null;
+        }
+
+        @Override
+        public boolean contains (Integer item){
+            return false;
+        }
+
+        @Override
+        public int indexOf (Integer item){
+            return 0;
+        }
+
+        @Override
+        public int lastIndexOf (Integer item){
+            return 0;
+        }
+
+        @Override
+        public String get ( int index){
+            return null;
+        }
+
+        @Override
+        public boolean equals (Listen otherList){
+            return false;
+        }
+
+        @Override
+        public int size () {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty () {
+            return false;
+        }
+
+        @Override
+        public void clear () {
+
+        }
+
+        @Override
+        public Integer[] toArray () {
+            return new Integer[0];
+        }
+    }
--- src/test/Main.java
+++ src/test/Main.java
@@ -1,7 +1,41 @@
 package test;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer[] arr1 = generateRandomArray();
+        Integer[] arr2 = arr1.clone();
+        Integer[] arr3 = arr1.clone();
+
+        long start = System.currentTimeMillis();
+        sortBubble(arr1);
+        System.out.println("Пузырьковая сортировка. Затраченное время - " + (System.currentTimeMillis() - start));
+
+        long start2 = System.currentTimeMillis();
+        sortSelection = (arr2);
+        System.out.println("Сортировка выбором. Затраченное время - " + (System.currentTimeMillis() - start2));
+
+        long start3 = System.currentTimeMillis();
+        sortInsertion = (arr3);
+        System.out.println("Сортировка вставками. Затраченное время - " + (System.currentTimeMillis() - start3));
+
     }
-}
+
+    public static Integer[] generateRandomArray() {
+        Random random = new Random();
+        Integer[] arr = new Integer[100000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100000) + 100000;
+        }
+        return arr;
+
+    }
+
+    private static void sortBubble(Integer[] arr1) {
+        for (int i = 0; i < arr.lenght-1; i++) {
+
+
+        }
+    }
+
